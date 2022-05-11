b1e704605d177fd4f1af3b9b3660434fc9379aad
LucasRoque021 <lucas.p.roque93@gmail.com>
2022-05-11 14:14:11 -0300

    MovieCard

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function Navbar(){
       <nav className='container'>
         <div className='dsmovie-nav-content'>
           <h1>DSMovie</h1>
-          <a href="https://github.com/LucasRoque021">
+          <a href="https://github.com/LucasRoque021" target="_blank">
             <div className='dsmovie-contact-container'>
               <GithubIcon/>
               <p className='dsmovie-contact-link'>/LucasRoque021</p>
