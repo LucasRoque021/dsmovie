b4b678658ae4c69a1096dc7cd1fb23b5a265b255
LucasRoque021 <lucas.p.roque93@gmail.com>
2022-05-15 02:56:17 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -16,7 +16,7 @@ function MovieCard({movie} : Props){
       <img src={movie.image} alt={movie.title} className="dsmovie-movie-card-image" />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore/>
+        <MovieScore score={movie.score} count={movie.count}/>
         <Link to={`/form/${movie.id}`}>
           <div className="btn btn-primary dsmovie-btn">Avaliar</div>
         </Link>
